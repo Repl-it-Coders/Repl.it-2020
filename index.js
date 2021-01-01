286ee67d680ec05abbe64e01bae2eb68ee2c05e5
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2021-01-01 07:59:34 +0000

    Changed to views

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ app.use('/static', express.static(__dirname + '/static'));
 
 app.get("/", async(req, res) => {
   console.log("Yeeet, [DEBUG] we running in home page");
-  res.sendFile(`${__dirname }/templates/home.html`);
+  res.sendFile(`${__dirname }/views/home.html`);
 });
 
 app.get("/posts", (_, res) => {
   console.log("We in the posts page [DEBUG]");
-  res.sendFile(`${__dirname }/templates/posts.html`);
+  res.sendFile(`${__dirname }/views/posts.html`);
 });
 
 app.get("/suggestions", (_, res) => {
   console.log("Hello, is this the debug center, yes, we are running the suggestions");
-  res.sendFile(`${__dirname }/templates/suggestions.html`);
+  res.sendFile(`${__dirname }/views/suggestions.html`);
 });
 
 app.get("/timeline", (_, res) => {
   console.log("Hello, we debugging the TIMELINE!!");
-  res.sendFile(`${__dirname }/templates/timeline.html`)
+  res.sendFile(`${__dirname }/views/timeline.html`)
 });
 
 app.listen(8080, () => {
