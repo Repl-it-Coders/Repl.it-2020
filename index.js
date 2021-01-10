c546b7d0eb269e5b6ceef7d7ba69e90f517482b3
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2021-01-10 06:15:18 +0000

    Don't know what changed

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
 
-// Finally got this line to work, so /static leads to any necessary static content. ~ SixBeeps
-app.use('/static', express.static(__dirname + '/static'));
-app.use('favicon.ico', express.static(__dirname + '/favicon.ico'))
+app.use("/static", express.static(__dirname + "/static"));
+
+app.use('/js', express.static(__dirname + '/js'));
+
+app.use("favicon.ico", express.static(__dirname + "/favicon.ico"));
+
+app.get("/login", async (_, res) => {
+	console.log(`Signing in [DEBUG]`);
+	res.sendFile(`${__dirname}/views/replAuth2.html`);
+});
+app.get("/home", async (_, res) => {
+	console.log(`Signing in [DEBUG]`);
+	res.sendFile(`${__dirname}/views/index.html`);
+});
 app.get("/", async (req, res) => {
   let username = req.get("X-REPLIT-USER-NAME");
   let loginURL = '#';
@@ -14,26 +26,86 @@ app.get("/", async (req, res) => {
 	console.log(`Yeeet, [DEBUG] we running in home page`);
 	res.send(fs.readFileSync(`/${__dirname}/views/home.html`).toString().split("{{USERNAME}}").join(username).split("{{LOGIN_URL}}").join(loginURL));
 });
-app.get("/", async(req, res) => {
-  console.log("[DEBUG] Connecting to home page.");
-  res.sendFile(`${__dirname }/views/index.html`);
+
+app.get("/posts", (req, res) => {
+  let username = req.get("X-REPLIT-USER-NAME");
+  let loginURL = '#';
+  if(!username || username.length == 0) {
+    username = "Log In";
+    loginURL = "/login";
+  }
+	console.log("We in the posts page [DEBUG]");
+	res.send(fs.readFileSync(`/${__dirname}/views/posts.html`).toString().split("{{USERNAME}}").join(username).split("{{LOGIN_URL}}").join(loginURL));
 });
 
-app.get("/posts", (_, res) => {
-  console.log("[DEBUG] Connecting to posts page.");
-  res.sendFile(`${__dirname }/views/posts.html`);
+app.get("/suggestions", (req, res) => {
+  
+  let username = req.get("X-REPLIT-USER-NAME");
+  let loginURL = '#';
+  let message = '';
+  if(!username || username.length == 0) {
+    username = "Log In";
+    loginURL = "/login";
+    message = "<h3>You must log in before submitting this form</h3>";
+  }
+	console.log(
+		"Hello, is this the debug center, yes, we are running the suggestions"
+	);
+	res.send(fs.readFileSync(`/${__dirname}/views/suggestions.html`).toString().split("{{USERNAME}}").join(username).split("{{LOGIN_URL}}").join(loginURL).split("{{MESSAGE}}").join(message));
 });
 
-app.get("/suggestions", (_, res) => {
-  console.log("[DEBUG] Connecting to suggestions page.");
-  res.sendFile(`${__dirname }/views/suggestions.html`);
+app.post("/api/suggestions", require("body-parser")(), (req, res) => {
+	console.log(
+		"Hello, is this the debug center, yes, we are running the suggestions API"
+	);
+	let body = req.body;
+	console.log("Username:", req.get("X-REPLIT-USER-NAME")); // Should we authenticate with Repl Auth? -- Yes we should ~ Whippingdot
+	console.log("Suggestion:", body.suggestion);
+
+	// We need to add a DB. ReplDB maybe? -- I don't know, we need a db or a file ~ Whippingdot
+	res.redirect("/suggestions");
 });
 
-app.get("/timeline", (_, res) => {
-  console.log("[DEBUG] Connecting to timeline page.");
-  res.sendFile(`${__dirname }/views/timeline.html`)
+app.get("/timeline", (req, res) => {
+  let username = req.get("X-REPLIT-USER-NAME");
+  let loginURL = '#';
+  if(!username || username.length == 0) {
+    username = "Log In";
+    loginURL = "/login";
+  }
+	console.log("We're in the timeline page [DEBUG]");
+	res.send(fs.readFileSync(`/${__dirname}/views/timeline.html`).toString().split("{{USERNAME}}").join(username).split("{{LOGIN_URL}}").join(loginURL));
 });
+
+// app.get("/home", async (_, res) => {
+// 	console.log(`Yeeet, [DEBUG] we running in home page`);
+// 	res.sendFile(`${__dirname}/views/home.html`);
+// });
+
+// app.get("/posts", (_, res) => {
+// 	console.log("We in the posts page [DEBUG]");
+// 	res.sendFile(`${__dirname}/views/posts.html`);
+// });
+
+// app.get("/suggestions", (_, res) => {
+// 	console.log(
+// 		"Hello, is this the debug center, yes, we are running the suggestions"
+// 	);
+// 	res.sendFile(`${__dirname}/views/suggestions.html`);
+// });
+
+// app.post("/api/suggestions", require("body-parser")(), (req, res) => {
+// 	console.log(
+// 		"Hello, is this the debug center, yes, we are running the suggestions API"
+// 	);
+// 	let body = req.body;
+// 	console.log("Username:", body.username); // Should we authenticate with Repl Auth? -- Yes we should ~ Whippingdot
+// 	console.log("Suggestion:", body.suggestion);
+
+// 	// We need to add a DB. ReplDB maybe? -- I don't know, we need a db or a file ~ Whippingdot
+// 	res.redirect("/suggestions");
+// });
 
 app.listen(8080, () => {
-  console.log("Listening on port 8080.");
+	console.log("Listening on port 8080.");
 });
